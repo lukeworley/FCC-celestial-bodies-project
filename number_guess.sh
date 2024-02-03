1ffa81ac01db42b13c8955310ac3fbbb7e021959
lukeworley <97200769+lukeworley@users.noreply.github.com>
2024-02-03 16:29:24 +0000

    Add files via upload

--- number_guess.sh
+++ number_guess.sh
@@ -3,6 +3,7 @@
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 RANDOM_NUMBER=$(( $RANDOM % 1000 + 1 ))
+echo $RANDOM_NUMBER
 
 echo Enter your username:
 read USERNAME
